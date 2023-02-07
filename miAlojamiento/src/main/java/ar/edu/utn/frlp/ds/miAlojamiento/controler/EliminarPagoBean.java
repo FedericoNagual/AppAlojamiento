c90a766c2f3d387862a32ae300db14e95be306cc
eztyloo <87723408+eztyloo@users.noreply.github.com>
2023-02-07 18:15:54 -0300

    eliminar  pago

---
+++ miAlojamiento/src/main/java/ar/edu/utn/frlp/ds/miAlojamiento/controler/EliminarPagoBean.java
@@ -0,0 +1,108 @@
+package ar.edu.utn.frlp.ds.miAlojamiento.controler;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import ar.edu.utn.frlp.ds.miAlojamiento.entidad.Pago;
+import ar.edu.utn.frlp.ds.miAlojamiento.entidad.DatosPago;
+import ar.edu.utn.frlp.ds.miAlojamiento.servicioImpl.ServicioPagoImpl;
+
+/**
+ * Clase Bean del Eliminar de Pago
+ * 
+ * @author Mauro
+ *
+ */
+@Named("eliminaPagoBean")
+@ViewScoped
+public class EliminarPagoBean extends GenericBean {
+
+	@Value("${eliminaPago}")
+	private Long id;
+
+	private Long idSeleccionado;
+	private List<DatosPago> datosCombo;
+	private Pago Pago;
+
+	@Autowired
+	private ServicioPagoImpl servicio;
+
+	/**
+	 * Se llama cuando se inicia la clase EliminarPagoBean que setea los datos
+	 * para usar y carga el combo box para seleccionar elq ue desea eliminar
+	 */
+	@PostConstruct
+	public void init() {
+		setDatosCombo(servicio.obtenerDatosPago());
+		setIdSeleccionado(null);
+		setPago(null);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getIdSeleccionado() {
+		return idSeleccionado;
+	}
+
+	public void setIdSeleccionado(Long idSeleccionado) {
+		this.idSeleccionado = idSeleccionado;
+	}
+
+	public List<DatosPago> getDatosCombo() {
+		return datosCombo;
+	}
+
+	public void setDatosCombo(List<DatosPago> datosCombo) {
+		this.datosCombo = datosCombo;
+	}
+
+	public Pago getPago() {
+		return Pago;
+	}
+
+	public void setPago(Pago Pago) {
+		this.Pago = Pago;
+	}
+
+	public ServicioPagoImpl getServicio() {
+		return servicio;
+	}
+
+	public void setServicio(ServicioPagoImpl servicio) {
+		this.servicio = servicio;
+	}
+
+	/**
+	 * Consulta de Pago por id y lo almacena en idSeleccionado
+	 * 
+	 */
+	public void consultaPago() {
+		if(idSeleccionado != null) {
+			setPago(servicio.buscarPagoXId(getIdSeleccionado()));
+		}
+	}
+
+	/**
+	 * Elimina un pago de la BD y muestra msj de exito
+	 */
+	public void eliminaPago() {
+		if(idSeleccionado != null) {
+			servicio.eliminarPago(getIdSeleccionado());
+			mostrarMensaje("Eliminacion con exito");
+		}
+		init();
+	}
+
+}
